c550e55b92ae8c32e67c71669a035941effc6024
Mandarancio <manda.mgf@gmail.com>
2020-02-21 12:00:11 +0100

    updated support utility

--- support.py
+++ support.py
@@ -12,7 +12,7 @@ APP.config.from_pyfile('config')
 DB = sqlite3.connect(APP.config.DB_FILE)
 DB.row_factory = sqlite3.Row
 
-TAGS = [] 
+TAGS = []
 RESULTS = []
 
 def __init_tags__():
@@ -29,29 +29,29 @@ def __init_results__():
         res[row['id']] = row['tag']
     return res
 
-def __get_test__(test_id):
+def get_test(test_id):
     rows = DB.execute(f"SELECT * FROM tests where id='{test_id}'")
     res = None
     for row in rows:
         res = dict(row)
     return res
 
-def __get_job__(job_id):
+def get_job(job_id):
     rows = DB.execute(f"SELECT * FROM jobs WHERE id = '{job_id}'")
     res = None
     for row in rows:
         res = dict(row)
-        res['dockerTag'] = __convert_tag__(res['dockerTag'])
-        res['result'] = __convert_result__(res['result'])
+        res['dockerTag'] = convert_tag(res['dockerTag'])
+        res['result'] = convert_result(res['result'])
     return res
 
-def __convert_tag__(tag_id):
+def convert_tag(tag_id):
     return {
         "id": tag_id,
         "name": TAGS[tag_id]
     }
 
-def __convert_result__(res_id):
+def convert_result(res_id):
     return {
         "id": res_id,
         "tag": RESULTS[res_id]
@@ -66,34 +66,53 @@ def __get_first_id__(table):
     return rows.fetchone()[0]
 
 
-def __get_id__(req, table):
+def get_id(req, table):
     res_id = None
     if isinstance(req, int):
         res_id = req
+        print("HERE")
     elif isinstance(req, str):
         low = req.lower()
         if low == 'last':
             res_id = __get_last_id__(table)
         if low == 'first':
             res_id = __get_first_id__(table)
+        if req.isdigit():
+            res_id = int(req)
     return res_id
 
+def get_test_id(test):
+    if isinstance(test, int):
+        return test
+    if isinstance(test, str):
+        if test.isdigit():
+            return int(test)
+        if test.lower() == 'last':
+            return __get_last_id__('tests')
+        if test.lower() == 'first':
+            return __get_first_id__('tests')
+        rows = DB.execute(f"SELECT id FROM tests where name='{test}'")
+        res = None
+        for row in rows:
+            res = row['id']
+        return res
+    return None
 
-def __get_job_stats__(job_id):
-    job = __get_job__(job_id)
+def get_job_stats(job_id):
+    job = get_job(job_id)
     if job is None:
         return text("Job do not exist", status=404)
 
     rows = DB.execute(f"""
-        SELECT 
-            id, test, job, result, start, duration, cpu_time, cpu_usage_avg, 
+        SELECT
+            id, test, job, result, start, duration, cpu_time, cpu_usage_avg,
             cpu_usage_max, memory_avg, memory_max, io_write, io_read, threads_avg
         FROM results WHERE job = '{job_id}' ORDER BY id""")
     res = []
     for row in rows:
         val = dict(row)
-        val['result'] = __convert_result__(val['result'])
-        val['test'] = __get_test__(val['test'])
+        val['result'] = convert_result(val['result'])
+        val['test'] = get_test(val['test'])
         val['job'] = job
         res.append(val)
 
