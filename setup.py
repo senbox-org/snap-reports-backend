93cfb5a981956f721dee8ab2e5ead5f7e39784d6
Martino Ferrari <manda.mgf@gmail.com>
2020-02-21 18:47:52 +0100

    more pythonic code

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
+"""
+Setup file for SNAP reports backend project.
+"""
 import setuptools
 from setuptools import setup
 
 with open("README.md", "r") as fh:
-    long_description = fh.read()
+    DESCRIPTION = fh.read()
 
 setup(
-   name='snap-reports-backend',
-   version='1.0',
-   description='A simple stat backend',
-   long_description=long_description,
-   long_description_content_type="text/markdown",
-   url="https://gitlab.com/mandarancio/snap-reports-backend",
-   author='Martino Ferrari',
-   author_email='martino.ferrari@c-s.fr',
-   packages=setuptools.find_packages(), 
-   install_requires=['sanic', 'numpy', 'matplotlib'],
-   scripts=[
-            'scripts/updatereferences.py'
-           ],
-   python_requires='>=3.6'
+    name='snap_reports_backend',
+    version='0.0.1',
+    description='A simple stat backend',
+    long_description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/mandarancio/snap-reports-backend",
+    author='Martino Ferrari',
+    author_email='martino.ferrari@c-s.fr',
+    packages=setuptools.find_packages(), 
+    install_requires=['sanic', 'numpy', 'matplotlib'],
+    scripts=['scripts/updatereferences.py'],
+    python_requires='>=3.6'
 )
