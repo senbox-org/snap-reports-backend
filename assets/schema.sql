a7f488129ae455137f25bf14c67e6cc16ebe776b
Martino Ferrari <manda.mgf@gmail.com>
2020-03-09 19:46:32 +0100

    added script to migrate sqlite data to mysql

--- assets/schema.sql
+++ assets/schema.sql
@@ -1,87 +1,100 @@
-CREATE TABLE "reference_values" (
-	"ID"	INTEGER,
-	"test"	INTEGER NOT NULL,
-	"referenceTag"	INTEGER NOT NULL,
-	"updated"	DATETIME NOT NULL,
-	"duration"	INTEGER NOT NULL,
-	"cpu_time"	INTEGER NOT NULL,
-	"cpu_usage_avg"	INTEGER NOT NULL,
-	"cpu_usage_max"	INTEGER NOT NULL,
-	"memory_avg"	INTEGER NOT NULL,
-	"memory_max"	INTEGER NOT NULL,
-	"io_write"	INTEGER NOT NULL,
-	"io_read"	INTEGER NOT NULL,
-	"threads_avg"	INTEGER NOT NULL,
-	"threads_max"	INTEGER NOT NULL,
-	"raw_data"	BLOB NOT NULL,
-	CONSTRAINT "UQ_reference" UNIQUE("test","referenceTag"),
-	FOREIGN KEY("referenceTag") REFERENCES "referenceTags"("ID"),
-	PRIMARY KEY("ID"),
-	FOREIGN KEY("test") REFERENCES "tests"("ID")
+DROP TABLE IF EXISTS results;
+DROP TABLE IF EXISTS reference_values;
+DROP TABLE IF EXISTS jobs;
+DROP TABLE IF EXISTS tests;
+DROP TABLE IF EXISTS dockerTags;
+DROP TABLE IF EXISTS resultTags;
+DROP TABLE IF EXISTS referenceTags;
+
+CREATE TABLE resultTags (
+	ID	INTEGER,
+	tag	VARCHAR(64) NOT NULL UNIQUE,
+	fatal	BOOLEAN NOT NULL,
+	PRIMARY KEY(ID)
 );
-CREATE TABLE "referenceTags" (
-	"ID"	INTEGER,
-	"tag"	VARCHAR(64) NOT NULL UNIQUE,
-	PRIMARY KEY("ID")
+
+CREATE TABLE dockerTags (
+	ID	INTEGER,
+	name	VARCHAR(64) NOT NULL UNIQUE,
+	PRIMARY KEY(ID)
 );
-CREATE TABLE "results" (
-	"ID"	INTEGER,
-	"test"	INTEGER NOT NULL,
-	"job"	INTEGER NOT NULL,
-	"result"	INTEGER NOT NULL,
-	"start"	DATETIME NOT NULL,
-	"duration"	INTEGER NOT NULL,
-	"cpu_time"	INTEGER NOT NULL,
-	"cpu_usage_avg"	INTEGER NOT NULL,
-	"cpu_usage_max"	INTEGER NOT NULL,
-	"memory_avg"	INTEGER NOT NULL,
-	"memory_max"	INTEGER NOT NULL,
-	"io_write"	INTEGER NOT NULL,
-	"io_read"	INTEGER NOT NULL,
-	"threads_avg"	INTEGER NOT NULL,
-	"threads_max"	INTEGER NOT NULL,
-	"raw_data"	BLOB NOT NULL,
-	"output" BLOB DEFAULT '',
-	CONSTRAINT "UQ_test" UNIQUE("test","job"),
-	FOREIGN KEY("result") REFERENCES "resultTags"("ID"),
-	FOREIGN KEY("job") REFERENCES "jobs"("ID"),
-	PRIMARY KEY("ID"),
-	FOREIGN KEY("test") REFERENCES "tests"("ID")
+
+CREATE TABLE referenceTags (
+	ID	INTEGER,
+	tag	VARCHAR(64) NOT NULL UNIQUE,
+	PRIMARY KEY(ID)
 );
-CREATE TABLE "tests" (
-	"ID"	INTEGER,
-	"name"	VARCHAR(256) NOT NULL UNIQUE,
-	"testset"	VARCHAR(256) NOT NULL,
-	"description"	VARCHAR(256) NOT NULL,
-	"author"	VARCHAR(256) NOT NULL,
-	"frequency"	VARCHAR(256) NOT NULL,
-	"graphPath"	VARCHAR(256) NOT NULL,
-	"graphXML" BLOB,
-	PRIMARY KEY("ID")
+
+CREATE TABLE tests (
+	ID	INTEGER,
+	name	VARCHAR(256) NOT NULL UNIQUE,
+	testset	VARCHAR(256) NOT NULL,
+	description	VARCHAR(256) NOT NULL,
+	author	VARCHAR(256) NOT NULL,
+	frequency	VARCHAR(256) NOT NULL,
+	graphPath	VARCHAR(256) NOT NULL,
+	graphXML MEDIUMBLOB,
+	PRIMARY KEY(ID)
 );
-CREATE TABLE "jobs" (
-	"ID"	INTEGER,
-	"branch"	VARCHAR(64) NOT NULL,
-	"jobnum"	INTEGER NOT NULL,
-	"dockerTag"	INTEGER NOT NULL,
-	"testScope"	VARCHAR(128) NOT NULL,
-	"timestamp_start"	DATETIME NOT NULL,
-	"timestamp_end"	DATETIME NOT NULL,
-	"result"	INTEGER NOT NULL,
-	CONSTRAINT "UQ_job" UNIQUE("branch","jobnum"),
-	FOREIGN KEY("result") REFERENCES "resultTags"("ID"),
-	FOREIGN KEY("dockerTag") REFERENCES "dockerTags"("ID"),
-	PRIMARY KEY("ID")
+
+CREATE TABLE jobs (
+	ID	INTEGER,
+	branch	VARCHAR(64) NOT NULL,
+	jobnum	INTEGER NOT NULL,
+	dockerTag	INTEGER NOT NULL,
+	testScope	VARCHAR(128) NOT NULL,
+	timestamp_start	DATETIME NOT NULL,
+	timestamp_end	DATETIME NOT NULL,
+	result	INTEGER NOT NULL,
+	CONSTRAINT UQ_job UNIQUE(branch,jobnum),
+	FOREIGN KEY(result) REFERENCES resultTags(ID),
+	FOREIGN KEY(dockerTag) REFERENCES dockerTags(ID),
+	PRIMARY KEY(ID)
 );
-CREATE TABLE "resultTags" (
-	"ID"	INTEGER,
-	"tag"	VARCHAR(64) NOT NULL UNIQUE,
-	"fatal"	BOOLEAN NOT NULL,
-	PRIMARY KEY("ID")
+
+CREATE TABLE reference_values (
+	ID	INTEGER,
+	test	INTEGER NOT NULL,
+	referenceTag	INTEGER DEFAULT 1,
+	updated	DATETIME NOT NULL,
+	duration	INTEGER NOT NULL,
+	cpu_time	INTEGER NOT NULL,
+	cpu_usage_avg	INTEGER NOT NULL,
+	cpu_usage_max	INTEGER NOT NULL,
+	memory_avg	INTEGER NOT NULL,
+	memory_max	INTEGER NOT NULL,
+	io_write	INTEGER NOT NULL,
+	io_read	INTEGER NOT NULL,
+	threads_avg	INTEGER NOT NULL,
+	threads_max	INTEGER NOT NULL,
+	raw_data	MEDIUMBLOB NOT NULL,
+	CONSTRAINT UQ_reference UNIQUE(test,referenceTag),
+	FOREIGN KEY(referenceTag) REFERENCES referenceTags(ID),
+	PRIMARY KEY(ID),
+	FOREIGN KEY(test) REFERENCES tests(ID)
 );
-CREATE TABLE "dockerTags" (
-	"ID"	INTEGER,
-	"name"	VARCHAR(64) NOT NULL UNIQUE,
-	PRIMARY KEY("ID")
+
+CREATE TABLE results (
+	ID	INTEGER,
+	test	INTEGER NOT NULL,
+	job	INTEGER NOT NULL,
+	result	INTEGER NOT NULL,
+	start	DATETIME NOT NULL,
+	duration	INTEGER NOT NULL,
+	cpu_time	INTEGER NOT NULL,
+	cpu_usage_avg	INTEGER NOT NULL,
+	cpu_usage_max	INTEGER NOT NULL,
+	memory_avg	INTEGER NOT NULL,
+	memory_max	INTEGER NOT NULL,
+	io_write	INTEGER NOT NULL,
+	io_read	INTEGER NOT NULL,
+	threads_avg	INTEGER NOT NULL,
+	threads_max	INTEGER NOT NULL,
+	raw_data	MEDIUMBLOB NOT NULL,
+	output 		MEDIUMBLOB,
+	CONSTRAINT UQ_test UNIQUE(test,job),
+	FOREIGN KEY(result) REFERENCES resultTags(ID),
+	FOREIGN KEY(job) REFERENCES jobs(ID),
+	PRIMARY KEY(ID),
+	FOREIGN KEY(test) REFERENCES tests(ID)
 );
-COMMIT;
--- scripts/move_to_mysql.py
+++ scripts/move_to_mysql.py
@@ -16,21 +16,57 @@ def __args__():
     return parser.parse_args()
 
 
-
 def init_mysql_db(connection):
     """
     Initialize MySQL database.
     """
     with open('assets/schema.sql', 'r') as schema_f:
-        schema = schema_f.read()
+        schema = schema_f.read().split(';')
         with connection.cursor() as cursor:
-            cursor.execute(schema)
+            for query in schema: 
+                if query:
+                    print(query)
+                    cursor.execute(query)
         connection.commit()
+
+
+def __val__(val):
+    if isinstance(val, bytes):
+        return "'"+val.decode('utf-8')+"'"
+    return "'"+str(val)+"'"
+
+
+def copy_table(table, source, target):
+    with target.cursor() as cursor:
+        print(f'== {table} ==')
+        src_query = f'SELECT * FROM {table};'
+        rows = source.execute(src_query)
+        keys = None
+        for i, row in enumerate(rows):
+            if keys is None:
+                keys = row.keys()
+            print(f'\rRow {i+1:>6}', end='')
+            query = f'INSERT INTO {table} ({", ".join(keys)}) '
+            query += f"VALUES ({', '.join([__val__(row[key]) for key in keys])})"
+            cursor.execute(query)
+    print()
+    target.commit()
+
+
+def copy_db(source, target):
+    copy_table('resultTags', source, target)
+    copy_table('dockerTags', source, target)        
+    copy_table('referenceTags', source, target)
+    copy_table('tests', source, target)
+    copy_table('jobs', source, target)
+    copy_table('results', source, target)
+    copy_table('reference_values', source, target)
 
 
 def __main__():
     args = __args__()
     src_db = sqlite3.connect(args.source)
+    src_db.row_factory = sqlite3.Row
     dbusr, dbpwd = args.target.split('@')[0].split(':')
     db_name = args.target.split('@')[1].split('/')[1]
     host, port = args.target.split('@')[1].split('/')[0].split(':')
@@ -43,6 +79,8 @@ def __main__():
         cursorclass=pymysql.cursors.DictCursor
     )
     init_mysql_db(trg_db)
+    copy_db(src_db, trg_db)
+
 
 if __name__ == '__main__':
     __main__()
