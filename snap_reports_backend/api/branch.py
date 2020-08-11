18e5145c23a2d7234fadbfe02cef8df13c7c60a0
Martino Ferrari <manda.mgf@gmail.com>
2020-08-11 10:01:13 +0200

    better branch name support

--- snap_reports_backend/api/branch.py
+++ snap_reports_backend/api/branch.py
@@ -17,10 +17,16 @@ def __init_result__():
     return {
         "count": 0,
         "improved": {
+            'duration': 0,
             'cpu': 0,
             'memory': 0,
             'read': 0,
             'both': 0,
+        },
+        "duration": {
+            "last": 0,
+            "last10": 0,
+            "average": 0,
         },
         "cpu": {
             "last": 0,
@@ -43,26 +49,29 @@ async def __stats_N__(cursor, tag, last=None):
     query = f"""
     SELECT 
         COUNT(results.ID), 
+        AVG((reference_values.duration - results.duration)/ reference_values.duration * 100), 
         AVG((reference_values.cpu_time - results.cpu_time)/ reference_values.cpu_time * 100), 
         AVG((reference_values.memory_avg - results.memory_avg)/reference_values.memory_avg * 100), 
         AVG((reference_values.io_read - results.io_read) / reference_values.io_read * 100),
+        SUM(reference_values.duration * 0.97 > results.duration),
         SUM(reference_values.cpu_time * 0.97 > results.cpu_time),
         SUM(reference_values.memory_avg * 0.97 > results.memory_avg),
         SUM(reference_values.io_read * 0.97 > results.io_read),
         SUM(reference_values.io_read * 0.97 > results.io_read AND 
             reference_values.memory_avg * 0.97 > results.memory_avg AND
-            reference_values.cpu_time * 0.97 > results.cpu_time),
+            reference_values.duration * 0.97 > results.duration),
+        SUM(reference_values.duration * 1.03 < results.duration),
         SUM(reference_values.cpu_time * 1.03 < results.cpu_time),
         SUM(reference_values.memory_avg * 1.03 < results.memory_avg),
         SUM(reference_values.io_read * 1.03 < results.io_read),
         SUM(reference_values.io_read  * 1.03 < results.io_read AND 
             reference_values.memory_avg * 1.03 < results.memory_avg AND
-            reference_values.cpu_time  * 1.03 < results.cpu_time)
+            reference_values.duration  * 1.03 < results.duration)
     FROM results 
     JOIN (
             SELECT ID from jobs where 
             dockerTag = 
-                (SELECT ID from dockerTags WHERE name='snap:{tag}')
+                (SELECT ID from dockerTags WHERE name='{tag}')
             ORDER BY ID DESC {'LIMIT '+str(last) if last else ''}
         ) jobs ON results.job IN (jobs.ID)
     INNER JOIN resultTags ON
@@ -76,27 +85,35 @@ async def __stats_N__(cursor, tag, last=None):
     values = list(row.values()) 
     return {
         'count': values[0],
-        'cpu': values[1],
-        'memory': values[2],
-        'read': values[3],
+        'duration': values[1],
+        'cpu': values[2],
+        'memory': values[3],
+        'read': values[4],
         'improved': {
-            'cpu': values[4],
-            'memory': values[5],
-            'read': values[6],
-            'both': values[7] 
+            'duration': values[5],
+            'cpu': values[6],
+            'memory': values[7],
+            'read': values[8],
+            'both': values[9] 
         },
         'regressed': {
-            'cpu': values[8],
-            'memory': values[9],
-            'read': values[10],
-            'both': values[11] 
+            'duration': values[10],
+            'cpu': values[11],
+            'memory': values[12],
+            'read': values[13],
+            'both': values[14] 
         }
     }
 
 
+def parse_tag(tag):
+    """Function to decode url tag"""
+    return tag.replace('%3A', ':')
+
 @branch.route("/<tag:string>/summary")
 async def get_branch_summary(_, tag):
     """Get branch statistics summary."""
+    tag = parse_tag(tag)
     conn = await DB.open()
     async with conn.cursor() as cursor:
         res = __init_result__()
@@ -110,7 +127,7 @@ async def get_branch_summary(_, tag):
         res['improved'] = data['last10']['improved']
         res['regressed'] = data['last10']['regressed']
 
-        for key in ('cpu', 'memory', 'read'):
+        for key in ('duration', 'cpu', 'memory', 'read'):
             res[key] = {
                 'last': data['last'][key],
                 'last10': data['last10'][key],
@@ -123,6 +140,7 @@ async def get_branch_summary(_, tag):
 @branch.route("/<tag:string>/summary/absolute")
 async def get_branch_summary_absolute(_, tag):
     """Get branch statistics absolute numbers."""
+    tag = parse_tag(tag)
     tests = await support.get_test_list(branch=tag)
     res = __init_result__()
     for test in tests:
@@ -148,6 +166,7 @@ async def get_branch_schduled_field_history(_, tag, field):
 
 @branch.route("/<tag:string>/history/scheduled/<field:string>/<window:int>")
 async def get_branch_scheduled_field_history_ma(_, tag, field, window):
+    tag = parse_tag(tag)
     result = await performances.get_branch_scheduled_field_history_moving_average(tag, field, window) 
     if  result is None:
         return text(f"Field `{field}` does not exist", status=500)
@@ -157,6 +176,7 @@ async def get_branch_scheduled_field_history_ma(_, tag, field, window):
 
 @branch.route("/<tag:string>/history/<field:string>")
 async def get_branch_field_history(_, tag, field):
+    tag = parse_tag(tag)
     result = await performances.get_branch_field_history(tag, field) 
     if  result is None:
         return text(f"Field `{field}` does not exist", status=500)
@@ -165,11 +185,13 @@ async def get_branch_field_history(_, tag, field):
 
 @branch.route("/<tag:string>/history/<field:string>/<window:int>")
 async def get_branch_field_history_ma(_, tag, field, window):
+    tag = parse_tag(tag)
     result = await performances.get_branch_field_history_moving_average(tag, field, window) 
     if  result is None:
         return text(f"Field `{field}` does not exist", status=500)
     dates, values = result
     return json({"date": dates, "value": values})
+
 
 async def __details_N__(tag, num):
     query = f"""
@@ -189,7 +211,7 @@ async def __details_N__(tag, num):
     JOIN (
         SELECT ID from jobs where 
         dockerTag = 
-            (SELECT ID from dockerTags WHERE name='snap:{tag}')
+            (SELECT ID from dockerTags WHERE name='{tag}')
         ORDER BY ID DESC {'LIMIT '+str(num) if num else ''}
     ) jobs on results.job In (jobs.ID)
     INNER JOIN reference_values ON
@@ -206,10 +228,10 @@ async def __details_N__(tag, num):
     return stats
 
 
-
 @branch.route("/<tag:string>/details/last")
 async def get_branch_details(_, tag):
     """Get branch statistics summary."""
+    tag = parse_tag(tag)
     query = f"""
     SELECT 
         tests.ID AS test_ID, tests.name AS name, 
@@ -227,7 +249,7 @@ async def get_branch_details(_, tag):
             SELECT test, max(job) AS lastJob 
             FROM results 
             WHERE job IN
-                (SELECT ID FROM jobs WHERE dockerTag = (SELECT ID FROM dockerTags WHERE name = 'snap:{tag}')) 
+                (SELECT ID FROM jobs WHERE dockerTag = (SELECT ID FROM dockerTags WHERE name = '{tag}')) 
             GROUP BY test
         ) filtr ON filtr.test = results.test AND filtr.lastJob = results.job
     INNER JOIN resultTags ON results.result = resultTags.ID
@@ -250,13 +272,14 @@ async def get_branch_details(_, tag, N):
 @branch.route("/<tag:string>/last_job")
 async def get_branch_last_job(_, tag):
     """Get last job of a given branch."""
+    tag = parse_tag(tag)
     row = await DB.fetchone(f'''
         SELECT jobs.ID, jobs.jobnum, jobs.timestamp_start, jobs.testScope, 
             resultTags.tag
         FROM jobs
         INNER JOIN resultTags ON jobs.result = resultTags.ID
         WHERE jobs.dockerTag =
-            (SELECT ID FROM dockerTags WHERE name='snap:{tag}')
+            (SELECT ID FROM dockerTags WHERE name='{tag}')
         ORDER BY jobs.ID DESC LIMIT 1;''')
     return json(row)
 
@@ -271,13 +294,14 @@ async def get_list(_):
 @branch.route("/<tag:string>/njobs")
 async def get_branch_njobs(_, tag):
     """Get number of jobs executed of a given branch."""
+    tag = parse_tag(tag)
     row = await DB.fetchone(f'''
         SELECT COUNT(ID) 
         FROM jobs
         WHERE dockerTag = (
             SELECT ID 
             FROM dockerTags
-            WHERE name='snap:{tag}'
+            WHERE name='{tag}'
         );''')
     return json({'njobs': row['COUNT(ID)']})
 
@@ -294,6 +318,8 @@ async def get_branch_comparison(request, tag_a, tag_b, field):
      - tag_b: second branch tag
      - field: field to examine (CPU Time, Memory etc)
     """
+    tag_a = parse_tag(tag_a)
+    tag_b = parse_tag(tag_b)
     if field.lower() not in FIELD_SET:
         return text(f"Field `{field}` does not exist", status=500)
     q_filter = ''
@@ -310,7 +336,7 @@ async def get_branch_comparison(request, tag_a, tag_b, field):
     FROM results
     INNER JOIN jobs ON jobs.ID = results.job
     WHERE 
-        jobs.dockerTag = (SELECT ID FROM dockerTags WHERE name='snap:{tag_a}')
+        jobs.dockerTag = (SELECT ID FROM dockerTags WHERE name='{tag_a}')
         AND
         results.result = (SELECT ID FROM resultTags WHERE tag='SUCCESS')
         AND
@@ -320,7 +346,7 @@ async def get_branch_comparison(request, tag_a, tag_b, field):
             FROM results
             INNER JOIN jobs ON jobs.ID = results.job
             WHERE 
-                jobs.dockerTag = (SELECT ID FROM dockerTags WHERE name='snap:{tag_b}')
+                jobs.dockerTag = (SELECT ID FROM dockerTags WHERE name='{tag_b}')
                 AND
                 results.result = (SELECT ID FROM resultTags WHERE tag='SUCCESS'))
     """
@@ -335,7 +361,7 @@ async def get_branch_comparison(request, tag_a, tag_b, field):
     JOIN (
         SELECT ID from jobs where 
         dockerTag = 
-            (SELECT ID from dockerTags WHERE name='snap:{tag_a}')
+            (SELECT ID from dockerTags WHERE name='{tag_a}')
         ORDER BY ID DESC
     ) jobs on results.job In (jobs.ID)
     INNER JOIN tests ON
@@ -361,7 +387,7 @@ async def get_branch_comparison(request, tag_a, tag_b, field):
     JOIN (
         SELECT ID from jobs where 
         dockerTag = 
-            (SELECT ID from dockerTags WHERE name='snap:{tag_b}')
+            (SELECT ID from dockerTags WHERE name='{tag_b}')
         ORDER BY ID DESC
     ) jobs on results.job In (jobs.ID)
     INNER JOIN tests ON
