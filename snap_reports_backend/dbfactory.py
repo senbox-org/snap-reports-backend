f58f3c09600386529d8443201c4615350228de7d
BALDO Frederick <frederick.baldo@csgroup.eu>
2024-01-11 10:39:43 +0100

    Update db connection

--- snap_reports_backend/dbfactory.py
+++ snap_reports_backend/dbfactory.py
@@ -164,4 +164,4 @@ def get_database():
         load_dotenv(sys.argv[1])
         return get_interface(os.getenv('DB_MODE'), os.getenv('DB'))
     else:
-        return get_interface('MYSQL', os.getenv('MYSQL_USER') + ':' + os.getenv('MYSQL_PASSWORD') + '@141.95.163.102:3306/' + os.getenv('MYSQL_DATABASE'))
+        return get_interface('MYSQL', os.getenv('MYSQL_USER') + ':' + os.getenv('MYSQL_PASSWORD') + '@51.91.81.10:3306/' + os.getenv('MYSQL_DATABASE'))
